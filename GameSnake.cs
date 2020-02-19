e0bbf43c8757fa124c79c938d7f70bd6444a7dab
Dileon <yevtayev@gmail.com>
2020-02-19 18:03:40 +0200

    My Snake

--- GameSnake.cs
+++ GameSnake.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Shapes;
 
 namespace Snake
 {
@@ -13,29 +14,32 @@ class GameSnake:Space
         Random rand = new Random();
         public GameSnake(byte x,byte y) : base(x,y)
         {
-           
             this.x = x;
             this.y = y;
             Console.Clear();
             SpaceBorders(ConsoleColor.Green);
-            SpaceFilling();
-            Action();
-        }
-        private void SpaceFilling()
-        {
             SpaceOutput(0);
+            Action();
         }
         private void Action()
         {
             Navigate n = Navigate.Default;
             ConsoleKeyInfo userInput;
-            int plX = (x / 2), plY = (PercentOfNum(90, y));
-            int delayTime = 300;//, delY = plY, delX = plX;
-            int snakeTailX = plX, snakeTailY = plY, snakeTailLangth = 1, counter = 0;
+            List<Snake> snake = new List<Snake>
+            {
+                new Snake(){x=20,y=20},
+                new Snake(){x=20,y=21},
+                new Snake(){x=20,y=22}
+            };
+            //Rectangle rec = new Rectangle();
+            //int plX = (x / 2), plY = (PercentOfNum(90, y));
+            int delayTime = 300;
             bool snakeСrashed = false;
             int score = 0;
-            while (plY > 1 && plY < y-1 && plX > 1 && plX < x-1 && snakeСrashed == false)
+            while (snake[0].y > 1 && snake[0].y < y-1 && snake[0].x > 1 && snake[0].x < x-1 && snakeСrashed == false)
             {
+                Snake mainSegment;
+                Snake secondSegment;
                 score++;
                 if (Console.KeyAvailable == true)
                 {
@@ -43,79 +47,53 @@ class GameSnake:Space
                     switch (userInput.Key)
                     {
                         case ConsoleKey.W:
-                            if (n == Navigate.Up)
-                            {
-                                delayTime = 150;
-                            }
-                            else
-                            {
-                                n = Navigate.Up;
-                                delayTime = 300;
-                            }
-                                
+                            n = Navigate.Up;
                             break;
                         case ConsoleKey.S:
-                            if (n == Navigate.Down)
-                                delayTime = 150;
-                            else
-                            {
-                                n = Navigate.Down;
-                                delayTime = 300;
-                            }
+                            n = Navigate.Down;
                             break;
                         case ConsoleKey.A:
-                            if (n == Navigate.Left) 
-                                delayTime = 150;
-                            else
-                            {
-                                n = Navigate.Left;
-                                delayTime = 300;
-                            }
+                            n = Navigate.Left;
                             break;
                         case ConsoleKey.D:
-                            if (n == Navigate.Right) 
-                                delayTime = 150;
-                            else
-                            {
-                                n = Navigate.Right;
-                                delayTime = 300;
-                            }
+                            n = Navigate.Right;
                             break;
                         default:
                             break;
                     }
                 }
-                //No keys pressed yet
-                plY = (n == Navigate.Default ? --plY : plY);
+
+                mainSegment = snake[0];
+
+                snake[0].y = (n == Navigate.Default ? --snake[0].y : snake[0].y);
                 //Pressed Up or Down 
-                plY = (n == Navigate.Up ? --plY : (n == Navigate.Down ? ++plY:plY));
+                snake[0].y = (n == Navigate.Up ? --snake[0].y : (n == Navigate.Down ? ++snake[0].y : snake[0].y));
                 //Pressed Right or Left 
-                plX = (n == Navigate.Left ? --plX : (n == Navigate.Right ? ++plX : plX));
-                //No keys pressed yet
-                // if (n == Navigate.Default) delY+=2;
-                //Stripping
-                //space[delY+1, delX] = '+';
-                space[plY, plX] = '@';
-                snakeTailLangth = 2;
-                //snakeTailY = (n == Navigate.Default ? snakeTailY+=snakeTailLangth : snakeTailY);
-                //Pressed Up or Down 
-                //snakeTailY = (n == Navigate.Up ? snakeTailY -= snakeTailLangth : (n == Navigate.Down ? snakeTailY += snakeTailLangth : snakeTailY));
-                //Pressed Right or Left 
-                //snakeTailX = (n == Navigate.Left ? snakeTailX -= snakeTailLangth : (n == Navigate.Right ? snakeTailX += snakeTailLangth : snakeTailX));
-                //snakeTailY += snakeTailLangth;
-                if (snakeTailY>y-1|| snakeTailY < 1)
+                snake[0].x = (n == Navigate.Left ? --snake[0].x : (n == Navigate.Right ? ++snake[0].x : snake[0].x));
+                for (int i = 1; i < snake.Count; i++)
                 {
-                    snakeTailY += 0;
-                }
-                else
-                {
-                    space[snakeTailY, snakeTailX] = '+';
+                        secondSegment = snake[i];
+                        snake[i] = mainSegment;
+                        mainSegment = secondSegment;
                 }
 
+
+                //Присваеваем значение остальным сегментам
+                //Присвоить значение колекцыи Snake
+                for (int i = 0; i < snake.Count; i++)
+                {
+                    space[snake[i].y, snake[i].x] = '@';
+                }
+
+                //No keys pressed yet
+                //plY = (n == Navigate.Default ? --plY : plY);
+                //Pressed Up or Down 
+                //plY = (n == Navigate.Up ? --plY : (n == Navigate.Down ? ++plY:plY));
+                //Pressed Right or Left 
+                //plX = (n == Navigate.Left ? --plX : (n == Navigate.Right ? ++plX : plX));
+                //space[plY, plX] = '@';
                 //snakeTailX=plY
                 SpaceOutput(score);
-                snakeTailY = plY;
-                snakeTailX = plX;
                 Thread.Sleep(delayTime);
             }
             GameSnake game = new GameSnake(50, 25);
